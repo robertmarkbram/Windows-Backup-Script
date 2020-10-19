cdbf69c41133200334632e2570661539c8b6f10a
Robert Mark Bram <robertmarkbram@gmail.com>
2020-10-19 20:28:09 +1100

    Windows backup script
    - Added readme, invisible.vbs, .gitignore.
    - Uploaded Backup.bat script.

---
+++ Backup.bat
@@ -0,0 +1,261 @@
+@ECHO OFF
+
+:: BACKUP DIRECTORIES AND DELETE OLD BACKUPS
+::
+:: Run this script without arguments and it will backup selected directories to
+:: a backup directory and delete old backup directories.
+
+:: SCHEDULE BACKUP TASKS
+:: Run this script with argument "sched" and it will schedule this script to run
+:: every few hours. You will need to enter your Windows password.
+:: For example:
+::    cd /D %HOMEPATH%\myApps\Windows-Backup-Script
+::    Backup.bat sched
+::    Scheduling task.
+
+:: Old blog entry about this.
+:: Directory backup utility.
+::    http://robertmarkbramprogrammer.blogspot.com.au/2010/06/script-to-backup-directories-on-regular_28.html
+
+:: - Tested on Win 10, 7 and XP 64 bit.
+:: - Dependency: invisible.vbs - small script that lets this batch file be run
+::    without popping up a DOS box. Is used when you schedule this batch.
+::    Put invisible.vbs in the same folder as this batch file.
+::
+::    The invisible.vbs file is included in this repo.
+::
+::    Or create it yourself. Contents of invisible.vbs:
+::       CreateObject("Wscript.Shell").Run """" & WScript.Arguments(0) & """", 0, False
+::
+:: - Dependency: forfiles.exe - Windows 7/XP-bit and later should have the
+::    FORFILES command by default. For earlier Windows builds, get it from:
+::      http://ss64.com/nt/forfiles.html
+::    and make sure to put forfiles.exe in the PATH (e.g. C:\WINDOWS\system32\
+::    for Windows XP).
+:: - Dependency: 7zip, a FREE and open source alternative to WinZip - get it
+::    from here: http://www.7-zip.org and edit the ZIP variable below if the
+::    path is NOT C:\Program Files\7-Zip\7z.exe.
+
+:: HISTORY
+:: =============================
+:: Wednesday, 23rd of June 2010, 5:10:55 PM
+:: - v1.
+:: Monday, 28th of June 2010, 6:03:28 PM
+:: - Modified to backup directories to zip files instead of backing 
+::    by copying directories.
+:: Monday, 12th of September 2011, 07:34:52 PM
+:: - Modified the way FORFILES is called to use / switches.
+::    Plus, added ZIP variable instead of relying on it being in the PATH.
+:: Saturday, 19th of May 2012, 11:43:42 AM
+:: - Integrated invisible.vbs to hide DOS box.
+:: Monday, 25th of April 2016, 04:39:57 PM
+:: - Re-wrote some comments.. trying to work out this error:
+::    ---------------------------
+::    Windows Script Host
+::    ---------------------------
+::    Script:   D:\apps\Batch\invisible.vbs
+::    Line:   1
+::    Char:   1
+::    Error:   The system cannot find the file specified.
+::    Code:   80070002
+::    Source:    (null)
+::    ---------------------------
+::    OK
+::    ---------------------------
+:: Thursday, 17th of May 2018, 11:04:27 AM
+:: - Uses specific path to find forfiles.exe
+:: Monday, 19th of October 2020, 05:52:17 PM
+:: - Fixed up usage of 7-zip: 
+::    - cd to folder we want to backup.
+::    - backup everyting except for exclude patterns.
+:: Monday, 19th of October 2020, 07:15:01 PM
+:: - Refactor to define backup configuration settings in a machine specific file:
+::    %HOMEPATH%\myApps\batch\backup-settings-for-%computername%.bat
+:: - Added extra comments and pretty to output.
+:: - Specified contents of invisible.vbs.
+
+:: =======================================
+:: EDIT THIS SECTION.
+:: =======================================
+:: Where is 7zip installed to?
+SET ZIP="C:\Program Files\7-Zip\7z.exe"
+:: Where is forfiles installed to?
+SET FORFILES="C:\Windows\System32\forfiles.exe"
+:: Define where to find settings for backup up files on this machine.
+:: By default looks for the settings file in the same folder as this script.
+SET BACKUP_SETTINGS_FILE=%~dp0\backup-settings-for-%computername%.bat
+
+:: =======================================
+:: DON'T CHANGE BELOW THIS POINT unless you know what you are doing!
+:: =======================================
+
+if not exist "%BACKUP_SETTINGS_FILE%" goto :SETTINGS_NOT_FOUND
+goto :LOAD_SETTINGS
+
+:: Load settings for this machine.
+:LOAD_SETTINGS 
+echo Loading setup configuration for this machine from:
+echo    %BACKUP_SETTINGS_FILE%
+call "%BACKUP_SETTINGS_FILE%"
+goto :POST_SETTINGS_LOADED
+
+:: Crap: couldn't load settings for this machine.
+:SETTINGS_NOT_FOUND
+echo DID NOT FIND A FILE CONFIGURING BACKUP SETTINGS FOR THIS MACHINE.
+echo.
+echo EXPECTED FILE: %BACKUP_SETTINGS_FILE%
+echo.
+echo.
+echo SAMPLE CONTENTS BELOW:
+echo.
+echo    :: Where do you want to put the backup files?
+echo    SET BACKUP_HOME=%%HOMEPATH%%\bak\workspace
+echo.   
+echo    :: How long (days) do you want to keep them?
+echo    SET DAYS_B4_DELETE=10
+echo.   
+echo    :: Define path and label (no spaces) for each directory you want to backup.
+echo    SET DIR1="C:\path\to\dir-1\to\backup"
+echo    SET LBL1=LABEL-1-NO-SPACE
+echo    SET DIR2="C:\path\to\dir-2\to\backup"
+echo    SET LBL2=LABEL-2-NO-SPACE
+echo    SET DIR3="%%HOMEPATH%%\path\to\dir-3\to\backup"
+echo    SET LBL3=LABEL-3-NO-SPACE
+echo.   
+echo    :: How many dir/lbl combinations did you define? Will ignore all beyond this number.
+echo    SET MAX=2
+echo.   
+echo    :: How often (in hours) do you want to run this script?
+echo    SET HOURS=4
+echo.   
+echo    :: What name to give the scheduled task.
+echo    SET SCHED_TASK_LABEL=Backup my PC every 4 hours
+echo.
+echo Create a file with contents like those above and try again.
+
+goto :END
+
+:: Logic that should occur once machine based settings are loaded.
+:POST_SETTINGS_LOADED
+
+:: Directory Checks
+IF "%BACKUP_HOME%" == "" (
+   echo Warning: BACKUP_HOME has not been set.
+   GOTO :END
+)
+IF NOT EXIST "%BACKUP_HOME%"  (
+   echo Warning: BACKUP_HOME [%BACKUP_HOME%] does not exist.
+   GOTO :END
+)
+
+:: Are we scheduling or backing up?
+IF "%1" == "sched" GOTO :SCHEDULE
+GOTO :BACKUP
+
+:: Schedule this script to run regularly - user will have to enter password.
+:SCHEDULE
+echo Scheduling task: %0.
+:: Schedule task.
+echo Scheduling task: "wscript.exe %~dp0invisible.vbs %~dp0%~n0"
+schtasks /create /SC HOURLY /MO %HOURS% /tn "%SCHED_TASK_LABEL%" /tr "wscript.exe %~dp0invisible.vbs %~dp0%~n0"
+GOTO :END
+
+:: Run backup tasks.
+:BACKUP
+echo.
+echo Backing up files.
+echo.
+
+:: Create timestamp.
+SET hh=%time:~0,2%
+if "%time:~0,1%"==" " SET hh=0%hh:~1,1%
+:: Home PC
+if "%computername%" == "ITS32675" (
+   echo yo
+   SET YYYYMMDD_HHMMSS=%date:~6,4%%date:~3,2%%date:~0,2%_%hh%%time:~3,2%%time:~6,2%
+) else (
+   SET YYYYMMDD_HHMMSS=%date:~10,4%%date:~7,2%%date:~4,2%_%hh%%time:~3,2%%time:~6,2%
+)
+
+:: Backup selected paths.
+:: Note use of ^^! below - to escape the exclamation mark because 
+::    we have turned on EnableDelayedExpansion
+::    and we want 7zip to see the exclamation mark, not DOS.
+SetLocal EnableDelayedExpansion
+For /L %%i in (1,1,%MAX%) Do (
+   IF EXIST !DIR%%i! (
+      echo.
+      echo .
+      echo ..
+      echo ...
+      echo ====
+      echo Backing up !DIR%%i!
+      echo ====
+      echo ...
+      echo ..
+      echo .
+      echo.
+      :: Go into the directory we want to backup.
+      cd !DIR%%i!
+
+      :: Backup everything except excluded files and directories.
+      %ZIP% a "%BACKUP_HOME%\!LBL%%i!_%YYYYMMDD_HHMMSS%" ^
+         -x^^!".git"           ^
+         -x^^!"noupload"       ^
+         -x^^!"out"            ^
+         -x^^!"target"         ^
+         -xr^^!".history"       ^
+         -xr^^!".databaseDumps"       ^
+         -x^^!"build"          ^
+         -x^^!"bin"            ^
+         -x^^!"node_modules"   ^
+         -xr^^!"*.bak"         ^
+         -xr^^!"*.log"         ^
+         -xr^^!"*.gz"          ^
+         -xr^^!"*.7z"          ^
+         -xr^^!"*.zip"
+   ) ELSE (
+      echo.
+      echo Specified backup directory does not exist. Skipping it.
+      echo    !DIR%%i!
+      echo.
+   ) 
+)
+EndLocal
+
+
+:: Delete old backups.
+echo.
+echo Deleting old files.
+echo.
+IF "%DAYS_B4_DELETE%" == "" (
+   echo WARNING! DAYS_B4_DELETE not set. Not deleting old backups.
+   GOTO :END
+)
+
+:: SetLocal EnableDelayedExpansion
+SetLocal EnableDelayedExpansion
+For /L %%i in (1,1,%MAX%) Do (
+   echo.
+   echo Looking for old files matching label: !LBL%%i!
+   %FORFILES% /P "%BACKUP_HOME%" /M !LBL%%i!_*.7z /D -%DAYS_B4_DELETE% ^
+      /C "CMD /C del /F /Q @FILE & echo Deleted @FILE "
+   echo.
+)
+EndLocal
+
+:: Use this for testing - echo what files will be deleted.
+:: SetLocal EnableDelayedExpansion
+:: For /L %%i in (1,1,%MAX%) Do (
+::    %FORFILES% /P "%BACKUP_HOME%" /M !LBL%%i!_*.7z /D -%DAYS_B4_DELETE% ^
+::       /C "CMD /C echo @FILE will be deleted"
+:: )
+:: EndLocal
+
+:END
+
+:: Uncomment the "pause" line if you want the command window to stick around
+:: until you "Press any key to continue . . ."
+:: (Let's you see the output of every run.)
+pause
+
--- README.md
+++ README.md
@@ -1,2 +1,91 @@
 # Windows-Backup-Script
+
 A batch script to backup folders on Windows.
+
+You get this script, then create your own `.bat` file that defines configuration parameters, such as which directories you want to back up.
+
+When you run this script, it will back up those directories (with certain exclusions) and delete old back-ups.
+
+You can also schedule this script to run every X hours.
+
+## How to set up the script
+
+How to set up this script.
+
+1. Git clone this repository somewhere.
+
+    ```bat
+    mkdir %HOMEPATH%\myApps
+    cd %HOMEPATH%\myApps
+    git clone git@github.com:robertmarkbram/Windows-Backup-Script.git
+    cd Windows-Backup-Script
+    ```
+
+2. Within the `Windows-Backup-Script` directory (or wherever you want), create a `.bat` file that controls how the backup script will operate.
+
+    Save the file as `backup-settings-for-%computername%.bat`, where `%computername%` is your computer's name. Find it by opening a DOS box and typing:
+
+    ```bat
+    echo %computername%
+    ```
+
+    It's named like this so the script doesn't have to be modified for whatever machine you run this on - it will look for a file with the same name as your machine. This script can be shared across multiple machines and will work the same as long as each have the same file based on their computer name.
+
+    **Configuration file.**
+
+    ```bat
+    :: Where do you want to put the backup files?
+    SET BACKUP_HOME=%HOMEPATH%\bak\workspace
+
+    :: How long (days) do you want to keep them?
+    SET DAYS_B4_DELETE=10
+
+    :: Define path and label (no spaces) for each directory you want to backup.
+    SET DIR1="C:\path\to\dir-1\to\backup"
+    SET LBL1=LABEL-1-NO-SPACE
+    SET DIR2="C:\path\to\dir-2\to\backup"
+    SET LBL2=LABEL-2-NO-SPACE
+    SET DIR3="%HOMEPATH%\path\to\dir-3\to\backup"
+    SET LBL3=LABEL-3-NO-SPACE
+
+    :: How many dir/lbl combinations did you define? Will ignore all beyond this number.
+    SET MAX=2
+
+    :: How often (in hours) do you want to run this script?
+    SET HOURS=4
+
+    :: What name to give the scheduled task.
+    SET SCHED_TASK_LABEL=Backup my PC every 4 hours
+    ```
+
+    This controls:
+
+    1. Where to save the back up files.
+    2. What directories to back-up.
+    3. What label to use when saving each backup (archives are named as: `LABEL_yyyymmdd_hhMMss.7z`)
+    4. How long to keep backups (in days).
+    5. What label to use when scheduling this task.
+3. If you choose to save the configuration file under some name other than `backup-settings-for-%computername%.bat` or in a different directory that the root directory of this project, you will need to edit `Backup.bat` and change this line:
+
+    ```bat
+    :: Define where to find settings for backup up files on this machine.
+    :: By default looks for the settings file in the same folder as this script.
+    SET BACKUP_SETTINGS_FILE=%~dp0\backup-settings-for-%computername%.bat
+    ```
+
+    Modify the path and file name to suit what you used.
+
+## How to run the script
+
+With your configuration file saved somewhere and `BACKUP_SETTINGS_FILE` modified if needed, run the script by double clicking on it.
+
+## How to schedule the script
+
+Open a command prompt. Change to the directory that holds this script and run the command `Backup.bat sched`.
+
+```bat
+cd /D %HOMEPATH%\myApps\Windows-Backup-Script
+Backup.bat sched
+```
+
+Depending on permissions on your machine, you may need to enter your password.
